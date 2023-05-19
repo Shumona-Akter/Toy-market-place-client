37ea6c5cb3cd25852cabaaaddf740a585880b114
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-05-19 10:42:20 +0600

    private route compelete

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { authContext } from '../Provider/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useContext(authContext)
+    const location = useLocation()
+    if(loading){
+        return <div className='text-center p-5 m-5'>
+            <Spinner  animation="border" variant="primary" />
         </div>
-    );
+    }
+   
+    if(user){
+        return children
+    }
+    console.log(user)
+    return <Navigate to="/login" state={{form:location}} replace></Navigate>
+
+   
 };
 
 export default PrivateRoute;
